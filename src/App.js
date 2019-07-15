a845f8103d95d10b3e0f1ffc52881140b0dcfdaf
Marcus Quettan <mquettan@gmail.com>
2019-07-14 22:13:07 -0400

    Attepting the hash router solution.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import React, { Component } from 'react';
 import Main from './components/Main/Main.js';
 import Candidates from './components/Candidates/Candidates.js';
 import Employers from './components/Employers/Employers.js';
-import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import { HashRouter, Route, Switch, Redirect } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
+      <HashRouter>
         <div className="App">
           <Switch>
             <Route exact path="/" component={Main}/>
@@ -16,7 +16,7 @@ class App extends Component {
             <Redirect to={{pathname: "/"}} />
           </Switch>
         </div>
-      </BrowserRouter>
+      </HashRouter>
     );
   }
 }
