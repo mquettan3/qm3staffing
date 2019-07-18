87873921cff1fb35f023f326097c1f10d53787fe
Marcus Quettan <marcus.quettan@gtri.gatech.edu>
2019-07-18 12:05:20 -0400

    Updating the language on the our process section of the main page.

--- src/components/Main/OurProcess.js
+++ src/components/Main/OurProcess.js
@@ -10,7 +10,6 @@ export default class OurProcess extends Component {
                       <h1 className="text-center mt-4">Our Process</h1>
                       <div className="separator"></div>
                       <p>What allows QM3 Solutions to attract talent and opportunities, that in-turn allows us to unite the right talent with the Right opportunity, is our 5 step process. This process attracts successful candidates as well as wonderful businesses with lucrative opportunities and becomes a true win win for all...</p>
-
                       <div id="accordion" className="collapse-style-2" role="tablist" aria-multiselectable="true">
                         <div className="card">
                           <div className="card-header" role="tab" id="headingOne">
@@ -22,9 +21,9 @@ export default class OurProcess extends Component {
                           </div>
                           <div id="collapseOne" className="collapse show" role="tabpanel" aria-labelledby="headingOne">
                             <div className="card-block light-gray-bg">
-                              The talent success of our candidates is the motivation behind our passion. It is actually the motivation behind everything we do. After all when they succeed, so do our partners. 
+                              The talent success of our candidates is the motivation behind our passion. Actually, it is the motivation behind everything we do. After all, when they succeed, so do our partners. 
                               <br/> <br/>
-                              To help us choose the Right talent for each opportunity we use mentoring and coaching, along with assessment testing. This process goes far beyond the traditional resume review and interview. We communicate the Talents wants, desires, and needs. Allowing us to bridge the gap between their true ability and the opportunity to excel with these abilities.
+                              To help us choose the right talent for each opportunity we use mentoring and coaching, along with assessment testing. This process goes far beyond the traditional resume review and interview. We communicate to determine what the individual wants, desires, and needs. Allowing us to properly identify and bridge the gap between their true ability and the perfect opportunity which would allow them to excel with their abilities.
                             </div>
                           </div>
                         </div>
@@ -38,7 +37,7 @@ export default class OurProcess extends Component {
                           </div>
                           <div id="collapseTwo" className="collapse" role="tabpanel" aria-labelledby="headingTwo">
                             <div className="card-block light-gray-bg">
-                              We put big ideas into even the smallest marketing campaigns to attract the right talent through coaching and mentoring unforeseen talent. We develop marketing strategies based on our comprehensive knowledge.
+                              We develop marketing strategies based on our comprehensive knowledge and put BIG ideas into even the smallest marketing campaigns.  We attract the right individual through extensive coaching and mentoring even unforeseen potential talent.
                             </div>
                           </div>
                         </div>
@@ -52,7 +51,7 @@ export default class OurProcess extends Component {
                           </div>
                           <div id="collapseThree" className="collapse" role="tabpanel" aria-labelledby="headingThree">
                             <div className="card-block light-gray-bg">
-                              We look beyond the surface and the fundamental job skills to find a persons true talents. Behavioral exploration helps us understand personalities, not just abilities. We seek to bring out the core values of an individual and transform them into a success.
+                              We look beyond the surface and the fundamental job skills to find a person's true talents. Behavioral exploration helps us understand personalities, not just abilities. We seek to bring out the core values of an individual and guide them into success.
                             </div>
                           </div>
                         </div>
@@ -66,7 +65,7 @@ export default class OurProcess extends Component {
                           </div>
                           <div id="collapseFour" className="collapse" role="tabpanel" aria-labelledby="headingFour">
                             <div className="card-block light-gray-bg">
-                              We perform behavioral, psychographic, fundamental skills, drug and background testing on each talent we connect with. Our proprietary process allows the Right talent to rise to the top and succeed.
+                              We perform behavioral, psychographic, fundamental skill, drug, and background testing on each individual we connect with. Our proprietary process allows the Right talent to rise to the top and succeed.
                             </div>
                           </div>
                         </div>
@@ -80,13 +79,13 @@ export default class OurProcess extends Component {
                           </div>
                           <div id="collapseFive" className="collapse" role="tabpanel" aria-labelledby="headingFive">
                             <div className="card-block light-gray-bg">
-                            Whether one of our partners has 1 or 100 opportunities, our proprietary talent search strategies deliver the right talents primed to succeed. Our 100% guarantee ensures that you have the right candidate on the job from day one, the right way and for all the right reasons. We make the process easy.
+                              Whether one of our partners has 1 or 100 opportunities, our proprietary talent search strategies deliver the right people primed to succeed. Our 100% guarantee ensures that you have the right candidate for all the right reasons on the job from day one. We make the process easy.
                             </div>
                           </div>
                         </div>
                       </div>
                       <br/>
-                      <p>QM3 Solutions strives to always have the Right Solution…. When selecting Talent to unite with our partners opportunities and needs, personal characteristics such as ethnicity, gender and sexual orientation are irrelevant. It is the core values of an individual that define a great talent. Values like; ambition, work ethic, and integrity.</p>
+                      <p>QM3 Solutions strives to always have the Right Solution... When selecting Talent to unite with our partner's opportunities and needs, personal characteristics such as ethnicity, gender and sexual orientation are irrelevant. It is the core values of an individual that define a great talent. Values like; ambition, work ethic, and integrity.</p>
                       <p>We work from coast to coast, delivering that hometown sensibility. We work together, wherever you are, to make your talent search easy.</p>
                     </div>
                   </div>
