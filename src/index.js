a7a56db55f5b803ac31a7499a939a7fbd1b67ca1
Marcus Quettan <mquettan@gmail.com>
2019-09-08 19:54:44 -0400

    Adding logrocket for error monitoring.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+// Error logging
+import LogRocket from 'logrocket';
+LogRocket.init('hwm11z/qm3-solutions');
+
 ReactDOM.render(
   <App />,
   document.getElementById('root')
